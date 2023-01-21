fbdfd61bcc9b029d7107ea8bc698a98f9d09cdeb
Александр Фомичев <fan160688@yandex.ru>
2023-01-21 21:09:27 +0300

    Test

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -23,7 +23,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'api',
-    'recipe',
+    'recipes',
     'users',
     'rest_framework',
     'rest_framework.authtoken',
