3e0f68a1218ae0af8c96136c5c54d97de718cb50
Александр Фомичев <fan160688@yandex.ru>
2023-01-22 16:11:47 +0300

    Test2

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -24,3 +24,13 @@ class ReadOnly(permissions.BasePermission):
 
     def has_permission(self, request, view):
         return request.method in permissions.SAFE_METHODS
+
+
+class IsAuthorOrAdminOrReadOnly(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return True
+
+    def has_object_permission(self, request, view, obj):
+        return (request.method in permissions.SAFE_METHODS
+                or obj.author == request.user
+                or request.user.is_staff)
