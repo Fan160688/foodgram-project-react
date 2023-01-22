948fa5d28c2e2dd8e785cbf050d52811b06456b3
Александр Фомичев <fan160688@yandex.ru>
2023-01-22 11:48:25 +0300

    Test2

--- backend/recipes/management/commands/import_data.py
+++ backend/recipes/management/commands/import_data.py
@@ -1,22 +1,23 @@
 import csv
 
 from django.conf import settings
-from django.core.management.base import BaseCommand
+from django.core.management.base import BaseCommand, CommandError
 from recipes.models import Ingredient
-
-TABLES_DICT = {
-    Ingredient: 'ingredients.csv'}
 
 
 class Command(BaseCommand):
     help = 'Загрузка данных из csv файлов'
 
-    def handle(self, *args, **options):
-        for model, base in TABLES_DICT.items():
+    def handle(self, *args, **kwargs):
+
+        try:
             with open(
-                f'{settings.BASE_DIR}/foodgram/static/data/{base}',
-                'r', encoding='UTF-8'
-            ) as csv_file:
-                reader = csv.DictReader(csv_file)
-                model.objects.bulk_create(model(**data) for data in reader)
-        self.stdout.write(self.style.SUCCESS('Данные успешно загружены'))
+                f'{settings.BASE_DIR}/foodgram/static/data/ingredients.csv', 'r', encoding='utf-8'
+            ) as file:
+                reader = csv.DictReader(file)
+                Ingredient.objects.bulk_create(
+                    Ingredient(**data) for data in reader
+                )
+            self.stdout.write(self.style.SUCCESS('Ингредиенты загружен'))
+        except FileNotFoundError:
+            raise CommandError('Добавьте файл ingredients в директорию data')
