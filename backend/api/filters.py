72ad3900929881ccc3cd4cbc08250b6bea41f171
Александр Фомичев <fan160688@yandex.ru>
2023-01-23 19:42:37 +0300

    test4

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -1,7 +1,7 @@
-import django_filters as filters
+from django_filters import rest_framework as filter
 from rest_framework.filters import SearchFilter
 
-from recipes.models import Recipe
+from recipes.models import Recipe, Tag
 from users.models import User
 
 
@@ -9,17 +9,32 @@ class IngredientSearchFilter(SearchFilter):
     search_param = 'name'
 
 
-class RecipeFilter(filters.FilterSet):
+class RecipeFilter(filter.FilterSet):
     "Фильтр рецептов"
-    author = filters.ModelChoiceFilter(
-        queryset=User.objects.all())
-    is_in_shopping_cart = filters.BooleanFilter(
-        widget=filters.widgets.BooleanWidget())
-    is_favorited = filters.BooleanFilter(
-        widget=filters.widgets.BooleanWidget)
-    tags = filters.AllValuesMultipleFilter(
-        field_name='tags__slug')
+    author = filter.CharFilter()
+    tags = filter.ModelMultipleChoiceFilter(
+        field_name='tags__slug',
+        queryset=Tag.objects.all(),
+        label='Tags',
+        to_field_name='slug'
+    )
+    is_favorited = filter.BooleanFilter(method='get_favorite')
+    is_in_shopping_cart = filter.BooleanFilter(
+        method='get_is_in_shopping_cart')
 
     class Meta:
         model = Recipe
-        fields = ['is_favorited', 'is_in_shopping_cart', 'author', 'tags']
+        fields = ('tags', 'author')
+
+    def get_favorite(self, queryset, name, value):
+        user = self.request.user
+        if value and not user.is_anonymous:
+            return queryset.filter(favorites__user=user)
+        return queryset
+
+    def get_is_in_shopping_cart(self, queryset, name, value):
+        user = self.request.user
+        if value and not user.is_anonymous:
+            return queryset.filter(shopping_list__user=user)
+        return queryset
+
