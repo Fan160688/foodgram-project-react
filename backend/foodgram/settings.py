7a5e066ecd288ddb629c30563ad15dbb6abdaf60
Александр Фомичев <fan160688@yandex.ru>
2023-01-26 05:27:42 +0300

    Ревью

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -63,7 +63,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'foodgram.wsgi.application'
 AUTH_USER_MODEL = 'users.User'
 
-#DATABASES = {
+# DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.sqlite3',
 #         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
